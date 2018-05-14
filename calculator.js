153b34263a96692d2ab568c3715268dc645cd9de
Kate Pond <kate@thepondsedge.com>
2018-05-14 16:46:02 -0700

    adds exponant feature

--- calculator.js
+++ calculator.js
@@ -6,6 +6,15 @@ const prompt = require('prompt');
 //then call the function `calculator` with the results
 
 // while calc = true keep calculating calc false quit
+
+const exponant = function exponant(num1, num2) {
+  let total = num1;
+  for (let i = 0; i < num2 - 1; i++) {
+    total *= num1
+  }
+  return total
+}
+
 prompt.start();
 
 prompt.get(['num1','num2','operation'], calculator)
@@ -26,6 +35,9 @@ function calculator(error, result) {
   } else if (result.operation == 'divide' || result.operation == '/') {
     const divide = parseFloat(result.num1) / parseFloat(result.num2)
     console.log(`${result.num1} / ${result.num2} = ${divide}`) ;
+
+  } else if (result.operation == 'exponant' || result.operation == '^') {
+    console.log(exponant(result.num1, result.num2));
 
   } else {
     console.log('null');
