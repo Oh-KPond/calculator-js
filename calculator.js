6fc58230d341f0d4d5761691582495f2d77d55be
Kate Pond <kate@thepondsedge.com>
2018-05-13 18:20:01 -0700

    solves the problem by using parseFloat to convert string to int

--- calculator.js
+++ calculator.js
@@ -10,19 +10,19 @@ prompt.get(['num1','num2','operation'], calculator)
 
 function calculator(error, result) {
   if (result.operation == 'add' || result.operation == '+') {
-    const add = Number(result.num1) + Number(result.num2)
+    const add = parseFloat(result.num1) + parseFloat(result.num2)
     console.log(`${result.num1} + ${result.num2} = ${add}`) ;
 
   } else if (result.operation == 'subtract' || result.operation == '-') {
-    const subtract = Number(result.num1) - Number(result.num2)
+    const subtract = parseFloat(result.num1) - parseFloat(result.num2)
     console.log(`${result.num1} - ${result.num2} = ${subtract}`) ;
 
   } else if (result.operation == 'multiply' || result.operation == '*') {
-    const multiply = Number(result.num1) * Number(result.num2)
+    const multiply = parseFloat(result.num1) * parseFloat(result.num2)
     console.log(`${result.num1} * ${result.num2} = ${multiply}`) ;
 
   } else if (result.operation == 'divide' || result.operation == '/') {
-    const divide = Number(result.num1) / Number(result.num2)
+    const divide = parseFloat(result.num1) / parseFloat(result.num2)
     console.log(`${result.num1} / ${result.num2} = ${divide}`) ;
 
   } else {
